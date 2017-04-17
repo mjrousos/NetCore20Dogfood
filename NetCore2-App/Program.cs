8e0616a4c7c8e99c5a7f7b4b7f222732b3346f2d
Mike Rousos <MikeRou@Microsoft.com>
2017-04-17 16:24:10 -0400

    Add simple .NET Core 2.0 project reference the NetStandard 2.0 library

---
+++ NetCore2-App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using NetStandard2_Libray;
+
+namespace NetCore2_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Serializing DateTime.Now with the binary formatter...");
+            var bytes = Serializer.Serialize(DateTime.Now);
+            Console.WriteLine($"Bytes: {string.Join(" ", bytes.Select(b => b.ToString("X2")))}");
+        }
+    }
+}
